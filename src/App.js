a2e2633967a7a0741e4a95b31c21fc42f9961a31
Ben Ukhanov <ben1zuk321@gmail.com>
2021-03-30 16:12:34 +0300

    Add react app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import ReactMarkdown from "react-markdown";
+import AboutMarkdown from "./assets/about.md";
+import ContactMarkdown from "./assets/contact.md";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      about: "",
+      contact: "",
+    };
+  }
+
+  componentWillMount() {
+    fetch(AboutMarkdown)
+      .then((res) => res.text())
+      .then((text) => this.setState({ about: text }));
+
+    fetch(ContactMarkdown)
+      .then((res) => res.text())
+      .then((text) => this.setState({ contact: text }));
+  }
+
+  render() {
+    const { about, contact } = this.state;
+
+    return (
+      <div className="App">
+        <ReactMarkdown source={about} />
+        <div className="Center">
+          <ReactMarkdown source={contact} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
